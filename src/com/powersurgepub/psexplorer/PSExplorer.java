ab05b209fbe47f09f2a6d32c10b89428e7c5e480
Herb Bowie <hbowie@mac.com>
2013-02-11 18:58:17 -0800

    Added Open Source Licensing.
    Adding Apache Version 2.0 Licensing.
    
    Signed-off-by: Herb Bowie <hbowie@mac.com>

--- src/com/powersurgepub/psexplorer/PSExplorer.java
+++ src/com/powersurgepub/psexplorer/PSExplorer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004 - 2013 Herb Bowie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.powersurgepub.psexplorer;
 
   import com.powersurgepub.xos2.*;
@@ -7,21 +23,7 @@ package com.powersurgepub.psexplorer;
 
 /**
    A Swing program that will retrieve and display important environmental
-   information. Used for debugging.<p>
-     
-   This code is copyright (c) 2004 by Herb Bowie of PowerSurge Publishing. 
-   All rights reserved. <p>
-   
-   Version History: <ul><li>
-      2004/07/21 - Created this class for use with Two Due.
-    </ul>
-   @author Herb Bowie 
-             (<a href="mailto:herb@powersurgepub.com">herb@powersurgepub.com</a>)<br>
-           of PowerSurge Publishing 
-             (<A href="http://www.powersurgepub.com/">www.powersurgepub.com/</a>)
-  
-   @version 
-      2004/07/21 - Created this class for use with Two Due.
+   information. Used for debugging.
  */
 public class PSExplorer 
     extends javax.swing.JFrame {
--- src/com/powersurgepub/psexplorer/PSFileFilter.java
+++ src/com/powersurgepub/psexplorer/PSFileFilter.java
@@ -1,10 +1,17 @@
 /*
- * PSFileFilter.java
+ * Copyright 2006 - 2013 Herb Bowie
  *
- * Created on November 1, 2006, 5:11 AM
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.powersurgepub.psexplorer;
