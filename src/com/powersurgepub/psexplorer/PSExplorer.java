05bdac79d957438ec3dcd164f9f5c7ca1822265f
Herb Bowie <hbowie@mac.com>
2013-12-01 13:37:58 -0800

    Corrected deprecated use of show method
    Signed-off-by: Herb Bowie <hbowie@mac.com>

--- src/com/powersurgepub/psexplorer/PSExplorer.java
+++ src/com/powersurgepub/psexplorer/PSExplorer.java
@@ -218,7 +218,7 @@ public class PSExplorer
    * @param args the command line arguments
    */
   public static void main(String args[]) {
-    new PSExplorer().show();
+    new PSExplorer().setVisible(true);
   }
   
   // Variables declaration - do not modify//GEN-BEGIN:variables
